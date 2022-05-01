04ce4dfae9f9fcd392e3024ed4d5cf93773abce2
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-05-01 03:23:45 -0500

    fix mobile review feature

--- components/review.tsx
+++ components/review.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 interface Props {
   by: string;
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 const Review: React.FC<Props> = ({ children, by }) => (
-  <section className="max-w-sm md:max-w-xl lg:max-w-3xl container mx-auto text-center justify-center px-5 md:px-10 lg:px-20">
-    <div className="text-xl md:text-2xl leading-10 lg:leading-[3rem] tracking-tight">
+  <section className="max-w-[15rem] md:max-w-xl lg:max-w-3xl container mx-auto text-center justify-center px-5 md:px-10 lg:px-20">
+    <div className="text-xl md:text-2xl text-center leading-10 lg:leading-[3rem] tracking-tight">
       &ldquo;{children}&rdquo;
       <div className="mt-6">&mdash; {by}</div>
     </div>
