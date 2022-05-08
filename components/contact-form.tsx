5cfb6676139226ef26dc1eab644bb4cc228f7915
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-05-07 20:05:43 -0500

    update design

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -2,14 +2,30 @@ import React from "react";
 
 const ContactForm: React.FC = () => {
   return (
-    <div className="bg-black text-white flex flex-col justify-center pt-10 pb-10 min-h-[70vh]">
+    <div className="bg-black text-white flex flex-col justify-center pt-0 pb-10 min-h-[70vh]">
+      <div className="pb-10 container mx-auto px-10 text-center items-center justify-center mt-28">
+        <div className="mt-10 grid gap-6 lg:gap-20">
+          <div className="">
+            <div className="container max-w-screen-lg mx-auto">
+              <img
+                alt="pictureofEdwin"
+                className="mx-auto object-cover mb-2 w-44 h-44 rounded-full"
+                src="/assets/social-media/me.JPG"
+              />
+            </div>
+            <div className="text-2xl xl:text-3xl">Edwin Hernandez</div>
+            
+            <h2 className="-mb-4"> Owner</h2>
+          </div>
+        </div>
+      </div>
       <div className="flex-1 flex flex-col justify-center items-center pt-10 lg:pt-6">
-        <div className="pb-10">
+        {/* <div className="pb-10">
           <div
             className="bg-gradient-to-r from-purple-600 to-blue-600 w-10 h-10 rounded-full"
           ></div>
-        </div>
-        <h2 className="text-4xl font-bold">Contact Us</h2>
+        </div> */}
+        <h2 className="text-4xl font-bold">Contact Me</h2>
         
         <form className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w-full lg:min-w-[500px]">
             <input 
