4e52695dd23496cc034402380922df751b3e8a09
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-06-18 23:17:19 -0500

    added transitions and animations

--- components/mashead.tsx
+++ components/mashead.tsx
@@ -1,5 +1,31 @@
 import React, { useRef, useContext, useState, useCallback } from "react";
 import { ScrollContext } from "../utils/scroll-observer";
+import { motion } from 'framer-motion';
+const easing = [.6, -.05, .01, .99]
+
+const fadeInUp = {
+  inital: {
+    y:60,
+    opacity: 0,
+  },
+  animate: {
+    y:0,
+    opacity: 1,
+    transition: {
+      duration: .6,
+      ease: easing,
+    }
+  }
+}
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+}
+
 
 const Masthead: React.FC = () => {
   const [imageLoaded, setImageLoaded] = useState(true);
@@ -18,7 +44,8 @@ const Masthead: React.FC = () => {
   }, []);
 
   return (
-    <div
+    <motion.div variants={stagger} 
+    animate={{ opacity:1}} initial={{ opacity:0}} transition={{delay: .2}}
       ref={refContainer}
       className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
       style={{
@@ -42,13 +69,13 @@ const Masthead: React.FC = () => {
         />
       </video>
 
-      <div className="p-12 font-bold z-10 text-white text-center flex-1 flex items-center justify-center flex-col">
+      <motion.div variants={fadeInUp} className="p-12 font-bold z-10 text-white text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 uppercase text-4xl xl:text-5xl">unt<a className="font-thin">Status</a></h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
           <span>Web Development,</span> <span>done right.</span>
         </h2>
-      </div>
-      <div
+      </motion.div>
+      <motion.div variants={fadeInUp}
         className={`animate-bounce flex-grow-0 pb-[100px] md:pb-20 transition-all duration-1000 z-10
             ${imageLoaded ? "opacity-100" : "opacity-0"}`}
       >
@@ -59,8 +86,8 @@ const Masthead: React.FC = () => {
           alt="scroll down"
           onLoad={handleImageLoaded}
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { motion } from 'framer-motion';
 import Head from 'next/head'
 import Masthead from '../components/mashead'
 import AboutUs from '../components/aboutus'
@@ -6,18 +7,40 @@ import Works from '../components/works'
 import Testimonials from '../components/testimonials'
 import Footer from '../components/footer'
 import ContactForm from '../components/contact-form'
-import Payment from '../components/payment'
 import Navbar from '../components/navbar'
+
+// animate: defines animation
+// initial: defines the inital state
+// exit: defines animation when component exits
+const easing = [.6, -.05, .01, .99]
+
+const fadeInUp = {
+  inital: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: .6,
+      ease: easing,
+    }
+  }
+}
+
 const Home: NextPage = (initialData:any) => {
 
   return (
-    <div className="scroll-smooth">
+    <motion.div
+    animate="animate" initial="inital"
+    exit={{ opacity: 0 }}  >
       <Head>
         <title>Welcome</title>
         <meta name="description" content="Made for the people, UNT." />
         <link rel="icon" href="/assets/logos/logo-100.svg" />
       </Head>
-      <Navbar />
+      <motion.div variants={fadeInUp} >
+        <Navbar />
+      </motion.div>
       <Masthead />
       <AboutUs />
       <Works />
@@ -25,7 +48,7 @@ const Home: NextPage = (initialData:any) => {
       <ContactForm />
       <Footer />
       
-    </div>
+    </motion.div>
   )
 }
 
