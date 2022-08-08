286a4a8556e7c91bec485aa0d93f10f23ead2c37
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-08-07 21:13:38 -0500

    created component for integration pages

---
+++ components/status.tsx
@@ -0,0 +1,89 @@
+import { motion } from "framer-motion";
+import React from "react";
+import style from "../styles/pages.module.css";
+
+const easing = [0.6, -0.05, 0.01, 0.99];
+const fadeInUp = {
+  inital: {
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+const fadeInDown = {
+  inital: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+const stagger = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const MainContainer1: React.FC<Props> = ({ children }) => (
+    <motion.div
+    variants={stagger}
+      animate="animate"
+      initial="inital"
+      exit={{ opacity: 0 }}>
+        {children}
+    </motion.div>
+);
+
+export const WorkContainer1: React.FC<Props> = ({ children }) => (
+  <div className="grid grid-cols-1 lg:grid-cols-2 w-full top-0 sticky">
+    {children}
+  </div>
+);
+
+export const WorkLeft1: React.FC<Props> = ({ children }) => (
+  <div
+    className={`${style.pagesBg} bg-opacity-100 saturate-100
+    flex flex-col items-center justify-center h-[30vh] lg:h-[91vh]
+    text-white text-4xl md:text-5xl font-semibold leading-10`}
+  >
+    <motion.div variants={fadeInUp}>
+        {children}
+    </motion.div>
+  </div>
+);
+
+export const WorkRight1: React.FC<Props> = ({ children }) => (
+  <div
+    className="bg-white text-black h-[57vh] md:h-[62vh] lg:h-[91vh] flex flex-1 lg:items-center 
+    text-center justify-center"
+  >
+    <motion.div variants={fadeInDown} className="text-2xl md:text-3xl w-full max-w-md pt-10 lg:pt-0 px-0 md:px-0">
+      {children}
+    </motion.div>
+  </div>
+);
+
+export const WorkAnimation1: React.FC<Props> = ({ children }) => (
+  <div className="flex flex-1 justify-center mb-[60px] h-[100px]">
+    {children}
+  </div>
+);
