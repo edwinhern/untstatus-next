113d6060ec484ccfc68f7e2415807a081615a493
Edwin Hernandez <Edwinhern.16@gmail.com>
2023-05-31 15:18:16 -0500

    Refactored and Decoupled About Component

--- components/About.Component.tsx
+++ components/About.Component.tsx
@@ -1,57 +1,31 @@
-import { ScrollContext } from "../src/utils/scroll-observer";
-import style from "../styles/About.module.css";
-import { useContext, useRef } from "react";
+import { useMemo } from "react";
+import TextBlock from "./About/TextBlock.Component";
+import { useScrollProgress } from "../src/hooks/useScrollProgress";
 
-const opacityForBlock = (sectionProgress: number, blockNo: number) => {
-  const progress = sectionProgress - blockNo;
-  if (progress >= 0 && progress < 1) return 1;
-  return 0.2;
+const calculateOpacity = (progress, blockNo) => {
+  const calculatedProgress = progress - blockNo;
+  return calculatedProgress >= 0 && calculatedProgress < 1 ? 1 : 0.2;
 };
 
 export const About: React.FC = () => {
-  const { scrollY } = useContext(ScrollContext);
-  const refContainer = useRef<HTMLDivElement>(null);
-
   const numOfPage = 3;
-  let progress = 0;
-  const { current: elContainer } = refContainer;
-  if (elContainer) {
-    const { clientHeight, offsetTop } = elContainer;
-    const screenH = window.innerHeight;
-    const halfH = screenH / 2;
-    const percentY =
-      Math.min(
-        clientHeight + halfH,
-        Math.max(-screenH, scrollY - offsetTop) + halfH
-      ) / clientHeight;
-    progress = Math.min(numOfPage - 0.5, Math.max(0.5, percentY * numOfPage));
-  }
-  return (
-    <section
-      id="about"
-      className="flex flex-col bg-black text-white py-5 lg:py-10"
-    >
-      <div
-        ref={refContainer}
-        className="tracking-normal font-bold container mx-auto px-10 lg:px-20 py-10 lg:py-20"
-      >
-        <p className="leading-snug mb-10 max-w-5xl mx-auto my-auto text-[32px] sm:text-[40px] md:text-[50px] lg:text-[60px]">
-          <span
-            className={`${style.aboutText}`}
-            style={{
-              opacity: opacityForBlock(progress, 0),
-            }}
-          >
+  const [progress, refContainer] = useScrollProgress(numOfPage);
+
+  const textBlocks = useMemo(
+    () => [
+      {
+        text: (
+          <>
             <strong className="text-blue-500">Stay ahead of the curve</strong>{" "}
             with our{" "}
             <strong className="text-green-500">cutting-edge project</strong>.
-          </span>
-          <span
-            className={`${style.aboutText} inline-block leading-snug after:content-['_']`}
-            style={{
-              opacity: opacityForBlock(progress, 1),
-            }}
-          >
+          </>
+        ),
+        blockNumber: 0,
+      },
+      {
+        text: (
+          <>
             We have been working diligently to bring this to fruition, fueled by
             the determination to prevent another incident like the{" "}
             <strong className="text-pink-500">
@@ -66,20 +40,42 @@ export const About: React.FC = () => {
               UNT Faculty, Staff, and Students
             </strong>
             .
-          </span>
-          <span
-            className={`${style.aboutText} leading-snug inline-block`}
-            style={{
-              opacity: opacityForBlock(progress, 2),
-            }}
-          >
+          </>
+        ),
+        blockNumber: 1,
+      },
+      {
+        text: (
+          <>
             Join us in our <strong className="text-green-500">mission</strong>{" "}
             to{" "}
             <strong className="text-pink-500">
               stay informed and prepared
             </strong>{" "}
             for the <strong className="text-purple-500">future</strong>.
-          </span>
+          </>
+        ),
+        blockNumber: 2,
+      },
+    ],
+    []
+  );
+
+  return (
+    <section
+      id="about"
+      className="flex flex-col bg-black text-white py-5 lg:py-10"
+    >
+      <div
+        ref={refContainer}
+        className="tracking-normal font-bold container mx-auto px-10 lg:px-20 py-10 lg:py-20"
+      >
+        <p className="leading-snug mb-10 max-w-5xl mx-auto my-auto text-[32px] sm:text-[40px] md:text-[50px] lg:text-[60px]">
+          {textBlocks.map((block) => (
+            <TextBlock opacity={calculateOpacity(progress, block.blockNumber)}>
+              {block.text}
+            </TextBlock>
+          ))}
         </p>
       </div>
     </section>
